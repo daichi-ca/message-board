0593cfba458d66cd073d99bad66c9bdb6b52af22
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-01-24 13:42:29 +0000

    CRUD messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,20 +4,48 @@ class MessagesController < ApplicationController
   end
 
   def show
+    @message = Message.find(params[:id])
   end
 
   def new
+    @message = Message.new
   end
 
   def create
+    @message =Message.new(message_params)
+    
+    if @message.save
+      flash[:success] = 'Messageが正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :new
+    end  
   end
 
   def edit
+    @message = Message.find(params[:id])
   end
 
   def update
+    @message = Message.find(params[:id])
+
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
   end
 end
+
+private
+
+ #Strong Parameter
+ def message_params
+   params.require(:message).permit(:content)
+ end
