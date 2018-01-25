f5fe11e63798af3cfecd88f978a1bbd361ec9cb3
daichi hagiwara <daichi.hagiwara@livesense.co.jp>
2018-01-25 06:52:49 +0000

    try db:seed function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
   def index
-    @messages = Message.all.page(params[:page]).per(3)
+    @messages = Message.all.page(params[:page])
   end
 
   def show
